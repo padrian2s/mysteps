326cb80018e24fbbfb32aee419a83544bb13f362
Adrian Paleacu <adrian.paleacu@gmail.com>
2026-03-24 17:44:45 +0200

    Bump version to 1.0.2 (versionCode 3) for Play Store release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.adrianp.mysteps"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     signingConfigs {
